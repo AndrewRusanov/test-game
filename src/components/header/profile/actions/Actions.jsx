4b59d4176456db2a4d7194da77fa6d6f59bbb6b0
Andrey Rusanov <rusanow.andrej2018@yandex.ru>
2023-10-10 22:59:15 +0300

    feat: add some adaptive

--- src/components/header/profile/actions/Actions.jsx
+++ src/components/header/profile/actions/Actions.jsx
@@ -4,10 +4,18 @@ import styles from './Actions.module.css';
 const Actions = () => {
   return (
     <div className={styles.actionsBtnContainer}>
-      <button className={styles.actionsBtn}>Редактировать</button>
-      <button className={styles.actionsBtn}>Импортировать</button>
-      <button className={styles.actionsBtn}>Экспортировать</button>
-      <button className={styles.actionsBtn}>Ударить персонажа</button>
+      <button className={styles.actionsBtn}>
+        <p className={styles.actionsBtnText}>Редактировать</p>
+      </button>
+      <button className={styles.actionsBtn}>
+        <p className={styles.actionsBtnText}>Импортировать</p>
+      </button>
+      <button className={styles.actionsBtn}>
+        <p className={styles.actionsBtnText}>Экспортировать</p>
+      </button>
+      <button className={styles.actionsBtn}>
+        <p className={styles.actionsBtnText}>Ударить персонажа</p>
+      </button>
     </div>
   );
 };
